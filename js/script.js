5e147b797d226cb1a135500173a21e4276ddf3d0
Andrea Miglietta <m.andrea96@hotmail.it>
2022-08-29 17:30:54 +0200

    numeri casuali stampati in console

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log('JS OK!!!')
+
+// random number from 1 to 6 for the player
+const playerScore = Math.floor(Math.random() * 6) + 1;
+console.log('Player win', playerScore);
+
+// random number from 1 to 6 for the computer
+const computerScore = Math.floor(Math.random() * 6) + 1;
+console.log('Computer win', computerScore);
+
+
+//exercise condition
+if (playerScore > computerScore) {                // if player number > computer number --> player win
+    console.log('Player win');
+} else if (playerScore < computerScore) {        // if player number < computer number --> computer win
+    console.log('Computer win');
+} else {
+    console.log('Draw');
+}
